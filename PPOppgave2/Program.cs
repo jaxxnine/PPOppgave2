a2e600d90a8c0c02b4d97174da1d645180ed41b4
jaxxnine <jaxxnine.artist@gmail.com>
2025-11-26 12:14:38 +0100

    update

--- PPOppgave2/Program.cs
+++ PPOppgave2/Program.cs
@@ -6,8 +6,26 @@ namespace PPOppgave2
     {
         static void Main(string[] args)
         {
-        Console.WriteLine("Who would you like to know about? (Jon/Janssen)");
-        Console.WriteLine("Write a name:");
+            Console.WriteLine("Who would you like to know about? (Jon/Janssen)");
+            Console.WriteLine("Write a name:");
+        
+            string input = Console.ReadLine()?.Trim().ToLower();
+        
+            User chosenUser = null;
+        
+            if (input == "jon")
+            {
+                chosenUser = User.Jon;
+            }
+            else if (input == "janssen")
+            {
+                chosenUser = User.Janssen;
+            }
+            else
+            {
+                Console.WriteLine("Invalid person, try again.");
+            return;
+            }
         }
     }
 }
--- PPOppgave2/obj/Debug/net10.0/PPOppgave2.AssemblyInfo.cs
+++ PPOppgave2/obj/Debug/net10.0/PPOppgave2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PPOppgave2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10f2254f9bcbf011b4be09b32bd9609429eb90cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("PPOppgave2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PPOppgave2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
